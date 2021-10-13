129e099ecba313ce26c35e56f584fe41799777a3
Jason Whisnant <tasups@gmail.com>
2021-10-13 20:18:43 +0000

    Slider done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Palette from './Palette.js';
 import seedColors from './seedColors.js';
+import { generatePalette } from './colorHelpers.js';
 
 function App() {
   return (
-    <div className="App">
-      <Palette {...seedColors[3]}/>
+    <div>
+      <Palette palette={generatePalette(seedColors[4])}/>
     </div>
   );
 }
