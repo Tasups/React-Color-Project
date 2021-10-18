51f863c225034c7742905856d8a60c5adbf786aa
Jason Whisnant <tasups@gmail.com>
2021-10-18 18:57:04 +0000

    Add Link to Palette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -7,7 +7,7 @@ const styles = {
     border: "1px solid black",
     borderRadius: "5px", 
     padding: "0.5rem",
-    paddingBottom: "2rem",
+    paddingBottom: "1.69rem",
     position: "relative",
     overflow: "hidden",
     "&:hover": {
@@ -59,7 +59,7 @@ function MiniPalette(props) {
   })
   
   return (
-    <div className={classes.root}>
+    <div className={classes.root} onClick={props.handleClick}>
       <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName}<span className={classes.emoji}>{emoji}</span>
