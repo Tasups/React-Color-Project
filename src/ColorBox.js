51500b6f9b10ce02467304931569dea9eec5491d
Jason Whisnant <tasups@gmail.com>
2021-10-19 00:13:48 +0000

    Display colors in SingleColorPalette

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -19,26 +19,31 @@ class ColorBox extends Component {
   }
   
   render() {
-    const { name, background, moreUrl } = this.props;
+    const { name, background, moreUrl, showLink } = this.props;
     const { copied } = this.state;
     return(
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-      <div style={{ background }} className="ColorBox">
-        <div style={{ background }} className={`copy-overlay ${copied && "show"}`}/>
-          <div className={`copy-msg ${copied && "show"}`}>
-            <h1>copied!</h1>
-            <p>{this.props.background}</p>
-          </div>
-          <div className="copy-container">
-            <div className="box-content">
-              <span>{ name }</span>
+      
+        <div style={{ background }} className="ColorBox">
+          <div style={{ background }} className={`copy-overlay ${copied && "show"}`}/>
+            <div className={`copy-msg ${copied && "show"}`}>
+              <h1>copied!</h1>
+              <p>{this.props.background}</p>
             </div>
-            <button className="copy-button">Copy</button>
-          </div>
-        <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-          <span className="see-more">More</span>
-        </Link>
-      </div>
+            <div className="copy-container">
+              <div className="box-content">
+                <span>{ name }</span>
+              </div>
+              <button className="copy-button">Copy</button>
+            </div>
+            {showLink && ( 
+            <Link to={moreUrl} onClick={e => e.stopPropagation()}>
+              <span className="see-more">More</span>
+            </Link>
+            )}
+          
+        </div>
+      
       </CopyToClipboard>
       )
   }
