38dd3f74da780f7c82ca80d2b6cad0553234b2a0
Jason Whisnant <tasups@gmail.com>
2021-10-20 11:59:00 +0000

    Add Drawer to NewPaletteForm

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -52,7 +52,7 @@ class PaletteList extends Component {
         <div className={classes.container}>
           <nav className={classes.nav}>
             <h1>React Colors</h1>
-            <Link to="/palette/new">Create New Palette</Link>
+            <Link to="/palette/new">Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => {
