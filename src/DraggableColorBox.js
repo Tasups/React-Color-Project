c8fb632247be50a218600ce48e5526d50492eb37
Jason Whisnant <tasups@gmail.com>
2021-10-20 20:25:03 +0000

    Style DraggableColorBox

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
+import DeleteSharpIcon from '@material-ui/icons/DeleteSharp';
+
 
 const styles = {
   root: {
@@ -10,16 +12,40 @@ const styles = {
     position: 'relative',
     cursor: 'pointer',
     marginBottom: '-3.5px',
+    "&:hover svg": {
+      color: "white",
+      transform: "scale(1.33)"
+    }
+  },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  deleteIcon: {
+    transition: "all 0.2s ease-in-out"
   }
 }
 
 function DraggableColorBox(props) {
+  const { classes } = props;
     return(
       <div 
-        className={props.classes.root} 
+        className={classes.root} 
         style={{backgroundColor: props.color}}
       >
-        {props.name}
+        <div className={props.classes.boxContent}>
+          <span>{props.name}</span>
+          <DeleteSharpIcon className={classes.deleteIcon} />
+        </div>
       </div>
       )
 }
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -5,7 +5,6 @@ import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
@@ -23,6 +22,8 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import List from '@material-ui/core/List';
+
 */
 
 const drawerWidth = 300;
